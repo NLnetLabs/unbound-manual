70402a448c40e69911aaa4dd4f851d15752bc052
Alex Band <alex@nlnetlabs.nl>
2021-12-17 12:08:45 +0100

    unbound(8) manpage in RST

--- source/conf.py
+++ source/conf.py
@@ -25,9 +25,9 @@ copyright = '2020-2022, NLnet Labs'
 author = 'NLnet Labs (CC-BY 3.0)'
 
 # The short X.Y version
-version = ''
+version = '1.14.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.14.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -148,9 +148,22 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'unboundusermanual', 'Unbound User Manual',
-     [author], 1)
+    ('manpages/unbound', 'unbound', 'Unbound DNS validating resolver',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 8),
+     ('manpages/unbound-checkconf', 'unbound-checkconf', 'Check unbound configuration file for errors.',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 8), 
+     ('manpages/unbound-anchor', 'unbound-anchor', 'Unbound anchor utility.',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 8),
+     ('manpages/unbound.conf', 'unbound.conf', 'Unbound configuration file.',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 5),
+     ('manpages/unbound-control', 'unbound-control', 'Unbound remote server control utility.',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 8),
+     ('manpages/unbound-host', 'unbound-host', 'Unbound DNS lookup utility',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 1),
+     ('manpages/libunbound', 'libunbound', 'Unbound DNS validating resolver functions.',
+     "Unbound developers are mentioned in the CREDITS file in the distribution.", 3)
 ]
+manpages_url = '{page}.html'
 
 # -- Options for Texinfo output ----------------------------------------------
 
