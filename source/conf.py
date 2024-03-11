fdcff27ded0f7f3c4525ab428f90e0d45f421948
Yorgos Thessalonikefs <yorgos@nlnetlabs.nl>
2024-03-11 14:37:00 +0100

    Update for 1.19.3

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ copyright = f'1999-{year}, NLnet Labs'
 author = 'NLnet Labs'
 
 # The short X.Y version
-version = '1.19.1'
+version = '1.19.3'
 # The full version, including alpha/beta/rc tags
 release = version
 
